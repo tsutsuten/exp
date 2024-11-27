efefaa6158d73f4a544b379a110a8a915f27ec7f
tsutsuten <ekusoken3214@gmail.com>
2024-11-27 19:03:47 +0900

    hello.js

--- routes/hello.js
+++ routes/hello.js
@@ -1,9 +1,9 @@
-var express = require('hello');
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hello', { title: 'hello' });
+  res.render('hello', { title: 'Hello Express' });
 });
 
 module.exports = router;
